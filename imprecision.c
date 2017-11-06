10518f419e45a04912aa017126d1c7aa67932995
Faith <fmugure@intracitygeeks.org>
2017-11-06 05:48:26 +0000

    update

--- imprecision.c
+++
@@ -1,8 +0,0 @@
-#include <cs50.h>
-#include <stdio.h>
-
-int main(void){
-
-    double imprecision = 1.0 / 10.0;
-    printf("%.100f\n", imprecision);
-}
--- overflow55.c
+++
@@ -1,9 +0,0 @@
-#include <cs50.h>
-#include <stdio.h>
-
-
-
-int main(void)
-{
-    printf("%.55f\n", 1.0/10.0);
-}
