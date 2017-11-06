6b66118f0b37946ae608948373d91e353e0d5f67
Faith <fmugure@intracitygeeks.org>
2017-11-06 05:45:50 +0000

    ownwork

---
+++ CS50OwnPractice/program1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int var_a = 4;
+  int var_b = 2;
+  printf("%d\n", var_a + var_b);
+  //return 0;
+}
---
+++ CS50OwnPractice/program2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    char text[] = "Hello!";
+    printf("%c\n", text[2]);
+    return 0;
+}
